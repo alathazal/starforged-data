32f695f05b59109cb85f7b8dacc9086a2d3d306a
Alathazal <alathazal@gmail.com>
2026-09-08 16:58:15 +0100

    updated tests

--- tests/JsonValidityTest.php
+++ tests/JsonValidityTest.php
@@ -3,6 +3,12 @@
 use Opis\JsonSchema\Schema;
 use Opis\JsonSchema\Validator;
 
-it('validates truths.json parses', function () {
-    expect(true)->toBe(true);
+describe('JSON Validity', function () {
+    it('truths.json parses correctly', function () {
+        $data = json_decode(
+            file_get_contents(__DIR__ . '/../src/data/truths.json')
+        );
+        
+        expect($data)->not->toBeNull();
+    });
 });
--- tests/SchemaValidationTest.php
+++ tests/SchemaValidationTest.php
@@ -3,32 +3,34 @@
 use Opis\JsonSchema\SchemaLoader;
 use Opis\JsonSchema\Validator;
 
-it('validates truths.json against the schema', function () {
-    $data = json_decode(
-        file_get_contents(__DIR__ . '/../src/data/truths.json')
-    );
-
-    expect($data)->not->toBeNull();
-
-    $shared_schema = json_decode(
-        file_get_contents(__DIR__ . '/../src/schema/shared.schema.json')
-    );
-
-    expect($shared_schema)->not->toBeNull();
-
-    $truths_schema = json_decode(
-        file_get_contents(__DIR__ . '/../src/schema/truths.schema.json')
-    );
-
-    expect($truths_schema)->not->toBeNull();
-
-    $validator = new Validator();
-    $validator->loader()->resolver()->registerRaw($shared_schema, 'schema:///shared.schema.json');
-
-    $result = $validator->validate($data, $truths_schema);
-
-    expect($result->isValid())
-        ->toBeTrue(
-            json_encode($result->error(), JSON_PRETTY_PRINT)
+describe('Validates data files against their schema', function() {
+    it('truths.json validates correctly', function () {
+        $data = json_decode(
+            file_get_contents(__DIR__ . '/../src/data/truths.json')
         );
+
+        expect($data)->not->toBeNull();
+
+        $shared_schema = json_decode(
+            file_get_contents(__DIR__ . '/../src/schema/shared.schema.json')
+        );
+
+        expect($shared_schema)->not->toBeNull();
+
+        $truths_schema = json_decode(
+            file_get_contents(__DIR__ . '/../src/schema/truths.schema.json')
+        );
+
+        expect($truths_schema)->not->toBeNull();
+
+        $validator = new Validator();
+        $validator->loader()->resolver()->registerRaw($shared_schema, 'schema:///shared.schema.json');
+
+        $result = $validator->validate($data, $truths_schema);
+
+        expect($result->isValid())
+            ->toBeTrue(
+                json_encode($result->error(), JSON_PRETTY_PRINT)
+            );
+    });
 });
